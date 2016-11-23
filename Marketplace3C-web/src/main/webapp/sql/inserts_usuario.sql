99c043fe65935b64ea343f738be33852b0a0e265
pc <pc@LAPTOP-PKR50LEP>
2016-11-22 22:46:47 -0500

    migracion jsp a primefaces consultar y crear proveedores

--- Marketplace3C-web/src/main/webapp/sql/inserts_usuario.sql
+++ Marketplace3C-web/src/main/webapp/sql/inserts_usuario.sql
@@ -1,3 +1,5 @@
 INSERT INTO `marketplace`.`usuario` (`ID_USUARIO`, `USUARIO`, `CONTRASENA`, `NOMBRE`, `ROL`) VALUES ('2', 'danilo', 'danilo', 'danilo', '2');
 INSERT INTO `marketplace`.`usuario` (`ID_USUARIO`, `USUARIO`, `CONTRASENA`, `NOMBRE`, `ROL`) VALUES (1, 'root', 'root', 'root', '1');
+INSERT INTO `marketplace`.`usuario` (`ID_USUARIO`, `USUARIO`, `CONTRASENA`, `NOMBRE`, `EMPRESA`, `NUMERO_DOCUMENTO`, `TIPO_DOCUMENTO`, `DIRECCION`, `CORREO`, `ESTADO`, `TELEFONO`, `ROL`) VALUES ('3', 'tribago', 'tribago', 'Tribago', 'Tribago', '11122333', '1', 'Calle 5', 'tribago@gmail.com', '1', '6548971', '3');
+
 commit;
